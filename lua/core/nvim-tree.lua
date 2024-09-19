ac469ba401f28757e026532451693d2efba6f209
Tsung Yu Chan <jkcharlie6679@gmail.com>
2024-09-20 01:35:08 +0800

    feat / open file in new tab

--- lua/core/nvim-tree.lua
+++ lua/core/nvim-tree.lua
@@ -12,6 +12,7 @@ local function on_attach(bufnr)
   vim.keymap.set("n", "C", api.tree.change_root_to_node, opts("CD"))
   vim.keymap.set("n", "v", api.node.open.vertical, opts("Open: Vertical Split"))
   vim.keymap.set("n", "h", api.node.open.horizontal, opts("Open: Horizontal Split"))
+  vim.keymap.set("n", "t", api.node.open.tab, opts("Open: New tab"))
 end
 
 function M.setup()
