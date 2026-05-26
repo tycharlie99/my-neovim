3437cec3b27c16c872d8f39a8fad9c49ee5f03e0
TsungYu Chan <tycharlie.c@gmail.com>
2026-05-26 23:27:18 +0800

    FEAT: add friendly snippets for cmp
      - load friendly-snippets into LuaSnip
      - enable snippet completion for cmp
      - improve snippet availability for common languages

--- lua/plugins/nvim-cmp.lua
+++ lua/plugins/nvim-cmp.lua
@@ -10,10 +10,12 @@ return {
       "hrsh7th/cmp-path",
       "L3MON4D3/LuaSnip",
       "saadparwaiz1/cmp_luasnip",
+      "rafamadriz/friendly-snippets",
     },
     config = function()
       local cmp = require("cmp")
       local luasnip = require("luasnip")
+      require("luasnip.loaders.from_vscode").lazy_load()
 
       cmp.setup({
         snippet = {
